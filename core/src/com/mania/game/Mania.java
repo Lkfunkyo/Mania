7de8d6d8905160e9adb9719ceeb1994387024494
Shonne <farrowdshonne@gmail.com>
2016-09-09 19:18:38 -0400

    Update

--- core/src/com/mania/game/Mania.java
+++ core/src/com/mania/game/Mania.java
@@ -6,21 +6,26 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mania.game.Screens.Assets;
+import com.mania.game.Screens.MenuScreen;
 import com.mania.game.Screens.PlayScreen;
 import com.mania.game.Screens.SplashScreen;
 
 public class Mania extends Game {
 
 	public SpriteBatch batch;
+	public Assets assets;
 
 	@Override
 	public void create () {
+		assets = new Assets();
 		batch = new SpriteBatch();
-		setScreen(new SplashScreen(this));
+		setScreen(new MenuScreen(this));
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		assets.dispose();
 	}
 }
