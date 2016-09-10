78547072eb4f6cc6225ce8036c3b8f48d13faffb
Lkfunkyo X <Lkfunkyo X>
2016-09-09 23:48:40 -0400

    many changes made

--- core/src/com/mania/game/Screens/SplashScreen.java
+++ core/src/com/mania/game/Screens/SplashScreen.java
@@ -1,10 +1,8 @@
 package com.mania.game.Screens;
 
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.math.Vector3;
 import com.mania.game.Constants;
 import com.mania.game.Mania;
-import com.mania.game.Sprites.Body;
 
 /**
  * Created by PC on 9/7/2016.
@@ -12,7 +10,6 @@ import com.mania.game.Sprites.Body;
 public class SplashScreen implements Screen {
 
     private Mania game;
-    private Body body;
 
     public SplashScreen(Mania game){
         this.game = game;
